50b8d9f4fbc6c60758f4ecb81c3937175521df7c
Emer John Niones <nionesemerjohn@gmail.com>
2022-09-19 14:23:31 +0800

    refactor, add restrict_access method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,18 @@
 class CategoriesController < ApplicationController
-    before_action :get_user
+    before_action :get_category, only: [:show, :edit, :update, :destroy]
+    before_action :restrict_access, only: [:show, :edit, :update, :destroy]
     
     def index
-        @categories = @user.categories.all
+        @categories = current_user.categories.all
         @count = @categories.count
     end
     
     def new
-        @category = @user.categories.build
+        @category = current_user.categories.build
     end
 
     def create
-        @category = @user.categories.build(category_params)
+        @category = current_user.categories.build(category_params)
         
         if @category.save
             redirect_to category_tasks_path(@category.id)
@@ -21,16 +22,14 @@ class CategoriesController < ApplicationController
     end
     
     def show
-        @category = @user.categories.find(params[:id])
+    
     end
 
     def edit
-        @category = @user.categories.find(params[:id])
+    
     end
     
     def update
-        @category = @user.categories.find(params[:id])
-
         if @category.update(category_params)
             redirect_to category_path(@category.id)
         else
@@ -39,15 +38,20 @@ class CategoriesController < ApplicationController
     end
 
     def destroy
-        @category = @user.categories.find(params[:id])
         @category.destroy
         redirect_to categories_path, status: :see_other
     end
 
     private
 
-    def get_user
-        @user = User.find(current_user.id)
+    def get_category
+        @category = Category.find(params[:id])
+    end
+
+    def restrict_access
+        if current_user != @category.user
+            redirect_to root_path
+        end
     end
 
     def category_params
